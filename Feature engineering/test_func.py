bae13718e07a7d6e6b5c4a621f500d592b71e1d5
rch1025 <70617665+rch1025@users.noreply.github.com>
2022-04-24 22:35:24 +0900

    Add files via upload

---
+++ Feature engineering/test_func.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def square(input_list):
+    return [x*x for x in input_list]
+
+
+# In[2]:
+
+
+def sss(input_list):
+    return [x+100 for x in input_list]
+
+
+# In[ ]:
+
+
+
+
